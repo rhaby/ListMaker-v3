308508db8d5e986aab1639d9af07ab7acc533034
rhaby <57067569+rhaby@users.noreply.github.com>
2020-07-28 13:40:56 +0300

    Add files via upload

---
+++ ListMaker-v3.py
@@ -0,0 +1,273 @@
+import os
+
+os.system("clear")
+
+__banner__ = """\033[1;33m |\   /| ListMaker-v2.1 ðŸ”¥â€¢
+\033[1;33m  \|_|/  Author: ArHaBy*
+  /. .\  Version: 2.1
+ =\_Y_/= Telegram: @rhaby
+  {>o<}  Insta: @iqk56
+
+=============================================="""
+print(__banner__)
+
+print ('ã€Šã€Šã€Šã€Šã€Šã€Šã€Š 1-  ListMaker  ã€‹ã€‹ã€‹ã€‹ã€‹ã€‹ã€‹')
+print('==============================================')
+print ('ã€Šã€Šã€Šã€Šã€Šã€Šã€Š 2- DeveloperMaker ã€‹ã€‹ã€‹ã€‹ã€‹ã€‹ã€‹')
+print('==============================================')
+print ('ã€Šã€Šã€Šã€Šã€Šã€Šã€Š 3- numberMaker  ã€‹ã€‹ã€‹ã€‹ã€‹ã€‹ã€‹')
+print('==============================================')
+print ('ã€Šã€Šã€Šã€Šã€Šã€Šã€Š 4- NumberIraq ðŸ‡®ðŸ‡¶Â° ã€‹ã€‹ã€‹ã€‹ã€‹ã€‹ã€‹')
+print('==============================================')
+print ('ã€Šã€Šã€Šã€Šã€Šã€Šã€Š 5- NumberAlgeria ðŸ‡©ðŸ‡¿Â° ã€‹ã€‹ã€‹ã€‹ã€‹ã€‹')
+print('==============================================')
+
+ali1 = input ("")
+if ali1 == '1' :
+    import random
+    import sys
+    import os
+    import time
+
+    os.system("rm list.txt")
+    os.system("clear")
+    print("                Author | arhaby")
+    time.sleep(1)
+    print('')
+    print("                Telegram | @rhaby")
+    time.sleep(1)
+    print('')
+    print("                ChannelTelegram | @ciku370")
+    time.sleep(1)
+    print('')
+    print("                List-Maker ArHaBy v2")
+    time.sleep(1)
+    print('=========================================')
+    print('$Example Domain email')
+    print('1-   @gmail.com')
+    print('1-   @hotmail.com')
+    print('1-   @yahoo.com')
+    print('1-   @outlook.com')
+    print('================ letsgo ================')
+    #================================================
+    uesr = input('username =ã€‹ã€‹ ')
+    rhaby1 = '1234567890qwertyuiopasdfghjklzxcvbnm'
+    email = input('Domain email =ã€‹ã€‹ ')
+    print('=========================================')
+    rhaby = input('What is a number List=ã€‹ã€‹ ')
+    rhaby = int(rhaby)
+    rhaby2 = input('Number of mattresses=ã€‹ã€‹ ')
+    rhaby2 = int(rhaby2)
+    print('==================================')
+        
+    for password in range(rhaby):
+        password = ''
+    
+    
+        for item in range(rhaby2):
+            rhaby3 = ''
+        for item in range(rhaby2):
+            rhaby3 += random.choice(rhaby1)
+
+
+
+        print (uesr+rhaby3+email)
+        with open('list.txt', 'a') as x:
+         x.write(uesr + rhaby3 + email + '\n')
+if ali1 == '3' :
+    import random
+    import sys
+    import os
+    import time
+
+    os.system("rm list.txt")
+    os.system("clear")
+    __banner__ = """    \033[1;33m |\   /| ListMaker-v2.1 ðŸ”¥â€¢
+    \033[1;33m  \|_|/  Author: ArHaBy*
+      /. .\  Version: 2.1
+     =\_Y_/= Telegram: @rhaby
+      {>o<}  Insta: @iqk56
+
+   """
+    print(__banner__)
+    print('================ letsgo ================')
+    #================================================
+    uesr = input('Domain number =ã€‹ã€‹ ')
+    rhaby1 = '1234567890'
+    print('=========================================')
+    rhaby = input('What is a number List=ã€‹ã€‹ ')
+    rhaby = int(rhaby)    
+    print('=========================================')
+    rhaby2 = input('Number of mattresses=ã€‹ã€‹ ')
+    rhaby2 = int(rhaby2)
+    print('==================================')
+
+    for password in range(rhaby):
+        password = ''
+
+
+        for item in range(rhaby2):
+            rhaby3 = ''
+        for item in range(rhaby2):
+            rhaby3 += random.choice(rhaby1)
+
+
+
+        print (uesr+rhaby3)
+        with open('list.txt', 'a') as x:
+         x.write(uesr + rhaby3 + '\n')
+if ali1 == '2' :
+    import random
+    import sys
+    import os
+    import time
+
+    os.system("rm list.txt")
+    os.system("clear")
+    
+    __banner__ = """    \033[1;33m |\   /| ListMaker-v2.1 ðŸ”¥â€¢
+    \033[1;33m  \|_|/  Author: ArHaBy*
+      /. .\  Version: 2.1
+     =\_Y_/= Telegram: @rhaby
+      {>o<}  Insta: @iqk56
+
+   """
+    print(__banner__)
+    
+    print('=========================================')
+    print('$Example Domain email')
+    print('1-   @gmail.com')
+    print('1-   @hotmail.com')
+    print('1-   @yahoo.com')
+    print('1-   @outlook.com')
+    print('=========================================')
+    print ("$Example type ABC 123 ._ ")
+    print ("1234567890qwertyuiopasdfghjklzxcvbnm._")
+    print('================ letsgo ================')
+    #================================================
+    uesr = input('username =ã€‹ã€‹ ')
+    print('=========================================')
+    rhaby1 = input('type ABC 123 ._ =ã€‹ã€‹ ')
+    print('=========================================')
+    email = input('Domain email =ã€‹ã€‹ ')
+    print('=========================================')
+    rhaby = input('What is a number List=ã€‹ã€‹ ')
+    rhaby = int(rhaby)
+    print('=========================================')
+    rhaby2 = input('Number of mattresses=ã€‹ã€‹ ')
+    rhaby2 = int(rhaby2)
+    print('==================================')
+
+    for password in range(rhaby):
+        password = ''
+
+
+        for item in range(rhaby2):
+             rhaby3 = ''
+        for item in range(rhaby2):
+            rhaby3 += random.choice(rhaby1)
+
+
+
+        print (uesr+rhaby3+email)
+        with open('list.txt', 'a') as x:
+         x.write(uesr + rhaby3 + email + '\n')
+if ali1 == '5' :
+    import random
+    import sys
+    import os
+    import time
+
+    os.system("rm list.txt")
+    os.system("clear")
+    __banner__ = """    \033[1;33m |\   /| ListMaker-v2.1 ðŸ”¥â€¢
+    \033[1;33m  \|_|/  Author: ArHaBy*
+      /. .\  Version: 2.1
+     =\_Y_/= Telegram: @rhaby
+      {>o<}  Insta: @iqk56
+
+   """
+    print(__banner__)
+    print('================ letsgo ================')
+    #==============================Ø§Ù„Ø¬Ø²Ø§Ø¦Ø±==================
+    uesr = '07' 
+    uesr2 = '05'
+    uesr3 = '06'
+    rhaby4 = '1234567890'
+
+    rhaby = input('What is a number List: ')
+    rhaby = int(rhaby)
+    rhaby2 = '8'
+    rhaby2 = int(rhaby2)
+
+    print('==================================')
+
+    for rhaby3 in range(rhaby):
+        rhaby3 = ''
+
+
+        for item in range(rhaby2):
+            rhaby3 = ''
+        for item in range(rhaby2):
+            rhaby3 += random.choice(rhaby4)
+
+
+
+        print (uesr+rhaby3)
+        print (uesr2+rhaby3)
+        print (uesr3+rhaby3)
+        with open('list.txt', 'a') as x:
+         x.write(uesr + rhaby3 + '\n')
+         x.write(uesr2 + rhaby3 + '\n')
+         x.write(uesr3 + rhaby3 + '\n')
+if ali1 == '4' :
+    import random
+    import sys
+    import os
+    import time
+
+    os.system("rm list.txt")
+    os.system("clear")
+    __banner__ = """    \033[1;33m |\   /| ListMaker-v2.1 ðŸ”¥â€¢
+    \033[1;33m  \|_|/  Author: ArHaBy*
+      /. .\  Version: 2.1
+     =\_Y_/= Telegram: @rhaby
+      {>o<}  Insta: @iqk56
+
+   """
+    print(__banner__)
+    print('================ letsgo ================')
+    #=========================Ø§Ù„Ø¹Ø±Ø§Ù‚=======================
+    uesr = '077' 
+    uesr2 = '079'
+    uesr3 = '078'
+    uesr4 = '075'
+    rhaby4 = '1234567890'
+
+    rhaby = input('What is a number List: ')
+    rhaby = int(rhaby)
+    rhaby2 = '8'
+    rhaby2 = int(rhaby2)
+
+    print('==================================')
+
+    for rhaby3 in range(rhaby):
+        rhaby3 = ''
+
+
+        for item in range(rhaby2):
+            rhaby3 = ''
+        for item in range(rhaby2):
+            rhaby3 += random.choice(rhaby4)
+
+
+
+        print (uesr+rhaby3)
+        print (uesr2+rhaby3)
+        print (uesr3+rhaby3)
+        print (uesr4+rhaby3)
+        with open('list.txt', 'a') as x:
+         x.write(uesr + rhaby3 + '\n')
+         x.write(uesr2 + rhaby3 + '\n')
+         x.write(uesr3 + rhaby3 + '\n')
+         x.write(uesr4 + rhaby3 + '\n')
